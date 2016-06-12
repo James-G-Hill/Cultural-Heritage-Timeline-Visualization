181d8535976530456464e4836dca76af0630d871
One <One@PC-One.lan>
2016-06-12 21:51:29 +0100

    Created the ThingCollections class & added a constructor plus add & count methods.

---
+++ TimelineVisualizerEntities/src/org/jghill/timelinevisualizerentities/ThingCollections.java
@@ -0,0 +1,41 @@
+package org.jghill.timelinevisualizerentities;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * Holds collections of other Entities, whether PhysicalThigns or other
+ * ThingCollections.
+ * 
+ * @author JGHill
+ */
+public class ThingCollections extends Entities {
+    
+    private String collectionName;
+    private String originalQuery;
+    
+    private final SortedSet<Entities> collection;
+    
+    public ThingCollections() {
+        collection = new TreeSet<>();
+    }
+    
+    /**
+     * Allows other entities to be added to this entity.
+     * 
+     * @param e another entity to be added.
+     */
+    public void addThing(Entities e) {
+        collection.add(e);
+    }
+    
+    /**
+     * Allows a count of the entities to be returned.
+     * 
+     * @return a count of the entities within the collection.
+     */
+    public int count() {
+        return collection.size();
+    }
+    
+}
