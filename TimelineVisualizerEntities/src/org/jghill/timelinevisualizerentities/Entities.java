ae9a65ddf12b7b0ed2526e4eebb871913415f717
One <One@PC-One.lan>
2016-06-12 21:22:42 +0100

    Created an abstract class for containing either objects or collections.

---
+++ TimelineVisualizerEntities/src/org/jghill/timelinevisualizerentities/Entities.java
@@ -0,0 +1,11 @@
+package org.jghill.timelinevisualizerentities;
+
+/**
+ * A class for representing collections & physical things as components of a
+ * composition.
+ * 
+ * @author JGHill
+ */
+abstract class Entities {
+    
+}
