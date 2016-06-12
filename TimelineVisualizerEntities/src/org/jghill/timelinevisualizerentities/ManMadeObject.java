2d00643f110e8f882de13d2c12a8a30184e8f592
One <One@PC-One.lan>
2016-06-12 19:30:46 +0100

    Created a concrete class for man-made objects.

---
+++ TimelineVisualizerEntities/src/org/jghill/timelinevisualizerentities/ManMadeObject.java
@@ -0,0 +1,10 @@
+package org.jghill.timelinevisualizerentities;
+
+/**
+ * A class representing objects that have been designed &  created by humans.
+ * 
+ * @author JGHill
+ */
+public class ManMadeObject extends PhysicalThing {
+    
+}
