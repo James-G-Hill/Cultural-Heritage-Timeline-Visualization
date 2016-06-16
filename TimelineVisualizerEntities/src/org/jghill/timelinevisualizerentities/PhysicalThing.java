84dbe38f03b9739b969bf4c9ef8e44fa4bbd6cb6
One <One@PC-One.lan>
2016-06-16 21:47:32 +0100

    Corrected missing 'public'.

--- TimelineVisualizerEntities/src/org/jghill/timelinevisualizerentities/PhysicalThing.java
+++ TimelineVisualizerEntities/src/org/jghill/timelinevisualizerentities/PhysicalThing.java
@@ -8,7 +8,7 @@ import java.util.Date;
  * 
  * @author JamesGHill
  */
-abstract class PhysicalThing extends Entities {
+public abstract class PhysicalThing extends Entities {
     
     /**
      * Dates
