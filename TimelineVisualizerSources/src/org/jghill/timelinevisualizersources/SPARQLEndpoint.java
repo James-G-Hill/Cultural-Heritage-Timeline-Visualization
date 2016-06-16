98164d401d20783840547e81007683f89c85efdf
One <One@PC-One.lan>
2016-06-16 22:13:48 +0100

    Created a constructor for a SPARQL endpoint source.

--- TimelineVisualizerSources/src/org/jghill/timelinevisualizersources/SPARQLEndpoint.java
+++ TimelineVisualizerSources/src/org/jghill/timelinevisualizersources/SPARQLEndpoint.java
@@ -10,6 +10,15 @@ public class SPARQLEndpoint extends Source {
     private String webAddress;
     
     /**
+     * Constructor for the SPARQL endpoint.
+     * 
+     * @param name A name for the source.
+     */
+    public SPARQLEndpoint(String name) {
+        super.setSourceName(name);
+    }
+    
+    /**
      * A getter class for returning the web address.
      */
     public String getWebAddress() {
