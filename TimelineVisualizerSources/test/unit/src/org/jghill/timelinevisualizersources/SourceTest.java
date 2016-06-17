86138adbf702ed7a59d87fc062f8a6f43d06675d
One <One@PC-One.lan>
2016-06-17 14:03:49 +0100

    Created a testing class for the Source class.

---
+++ TimelineVisualizerSources/test/unit/src/org/jghill/timelinevisualizersources/SourceTest.java
@@ -0,0 +1,47 @@
+package org.jghill.timelinevisualizersources;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * A testing class for the abstract Source class.
+ * 
+ * @author JGHill
+ */
+public class SourceTest {
+    
+    public SourceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of setSourceName method, of class Source.
+     */
+    @Test
+    public void testSetSourceName() {
+    }
+    
+    public class SourceImpl extends Source {
+        
+    }
+    
+}
