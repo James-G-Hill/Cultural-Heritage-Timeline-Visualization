1cddb1b705e466d5692bb6faa305c187ed9b8e2c
One <One@PC-One.lan>
2016-06-17 14:23:39 +0100

    Created a testing class for the SPARQL Endpoint class.

---
+++ TimelineVisualizerSources/test/unit/src/org/jghill/timelinevisualizersources/SPARQLEndpointTest.java
@@ -0,0 +1,52 @@
+package org.jghill.timelinevisualizersources;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Testing class for the SPARQL Endpoint class.
+ * 
+ * @author JGHill
+ */
+public class SPARQLEndpointTest {
+    
+    public SPARQLEndpointTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getWebAddress method, of class SPARQLEndpoint.
+     */
+    @Test
+    public void testGetWebAddress() {
+        
+    }
+
+    /**
+     * Test of setWebAddress method, of class SPARQLEndpoint.
+     */
+    @Test
+    public void testSetWebAddress() {
+        
+    }
+    
+}
