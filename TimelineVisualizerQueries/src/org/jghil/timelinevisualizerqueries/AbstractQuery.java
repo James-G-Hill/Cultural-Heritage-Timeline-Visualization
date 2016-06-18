909419653ffaa014d8511095853c6540d2a17dac
One <One@PC-One.lan>
2016-06-18 21:03:32 +0100

    Modified class name to not clash with the Apache ARQ 'Query' class.

---
+++ TimelineVisualizerQueries/src/org/jghil/timelinevisualizerqueries/AbstractQuery.java
@@ -0,0 +1,31 @@
+package org.jghil.timelinevisualizerqueries;
+
+import java.util.Date;
+
+/**
+ * An abstract class representing generic queries.
+ * 
+ * @author JGHill
+ */
+public abstract class AbstractQuery {
+    
+    private String queryName;
+    private Date lastRunDate;
+    
+    public String getQueryName() {
+        return queryName;
+    }
+    
+    public void setQueryName(String name) {
+        queryName = name;
+    }
+    
+    public Date getLastRunDate() {
+        return lastRunDate;
+    }
+    
+    public void setLastRunDate(Date lastRun) {
+        lastRunDate = lastRun;
+    }
+    
+}
