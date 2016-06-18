f44cdbead723c283ea5a6f191c1afd9e9484230b
One <One@PC-One.lan>
2016-06-18 21:22:46 +0100

    Added an import for the apache jena query class and corrected incorrect constructor parameter types.

---
+++ TimelineVisualizerQueries/src/org/jghil/timelinevisualizerqueries/SPARQLQuery.java
@@ -0,0 +1,23 @@
+package org.jghil.timelinevisualizerqueries;
+
+import org.apache.jena.query.*;
+
+/**
+ * A concrete implementation of the Query class representing a SPARQL query.
+ * 
+ * @author JGHill
+ */
+public class SPARQLQuery extends AbstractQuery {
+    
+    Query sparqlQuery;
+    
+    public SPARQLQuery(Query q) {
+        sparqlQuery = q;
+    }
+    
+    public SPARQLQuery(Query q, String name) {
+        sparqlQuery = q;
+        super.setQueryName(name);
+    }
+    
+}
