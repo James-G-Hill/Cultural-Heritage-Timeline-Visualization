bfefd542451a91553f15f5d59a720a3c69f8d07a
One <One@PC-One.lan>
2016-06-19 20:48:14 +0100

    Created an installer so that the SourceCollection can b e created on startup.

---
+++ TimelineVisualizerSources/src/org/jghill/timelinevisualizersources/Installer.java
@@ -0,0 +1,17 @@
+package org.jghill.timelinevisualizersources;
+
+import org.openide.modules.ModuleInstall;
+
+/**
+ * An installer for setting up the Source Collection for use.
+ * 
+ * @author JGHill
+ */
+public class Installer extends ModuleInstall {
+
+    @Override
+    public void restored() {
+        
+    }
+
+}
