18301b169bbac70d7ace1cedeba4e718bfeb6607
One <One@PC-One.lan>
2016-06-20 21:04:00 +0100

    Added base table model for the management tool window.

---
+++ TimelineVisualizerSources/src/org/jghill/timelinevisualizersourcesgui/SourceTableModel.java
@@ -0,0 +1,27 @@
+package org.jghill.timelinevisualizersourcesgui;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ * A table model for sources to feed into the source management table.
+ * 
+ * @author JGHill
+ */
+public class SourceTableModel extends AbstractTableModel {
+
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
