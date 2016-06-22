0526f8603687d493e491f50f0a7a479d58575eed
One <One@PC-One.lan>
2016-06-22 22:25:58 +0100

    Initial setup of a TableModel.

--- TimelineVisualizerSources/src/org/jghill/timelinevisualizersourcesgui/SourceTableModel.java
+++ TimelineVisualizerSources/src/org/jghill/timelinevisualizersourcesgui/SourceTableModel.java
@@ -1,6 +1,8 @@
 package org.jghill.timelinevisualizersourcesgui;
 
+import java.util.SortedSet;
 import javax.swing.table.AbstractTableModel;
+import org.jghill.timelinevisualizersources.Source;
 
 /**
  * A table model for sources to feed into the source management table.
@@ -9,19 +11,44 @@ import javax.swing.table.AbstractTableModel;
  */
 public class SourceTableModel extends AbstractTableModel {
 
+    private final Source[] sources;
+    
+    public SourceTableModel(SortedSet<Source> collection) {
+        sources = (Source[]) collection.toArray();
+    }
+    
     @Override
     public int getRowCount() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//        return sources.length;
     }
 
     @Override
     public int getColumnCount() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//        return 2;
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//        switch(columnIndex) {
+//            case 0:
+//                return sources[rowIndex].getSourceName();
+//            case 1:
+//                return sources[rowIndex].getSourceType();
+//            default:
+//                return "";
+//        }
+    }
+    
+    @Override
+    public String getColumnName(int col) {
+//        switch (col) {
+//            case 0:
+//                return "Name";
+//            case 1:
+//                return "Type";
+//            default:
+//                return "";
+//        }
     }
     
 }
