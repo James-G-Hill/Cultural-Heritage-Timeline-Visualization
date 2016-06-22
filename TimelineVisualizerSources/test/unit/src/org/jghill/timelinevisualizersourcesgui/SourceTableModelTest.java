3ac6121cdd6e1b9be03c39c387eb61b89ed47e75
One <One@PC-One.lan>
2016-06-22 22:19:02 +0100

    Created a testing class for the SourceTableModel.

---
+++ TimelineVisualizerSources/test/unit/src/org/jghill/timelinevisualizersourcesgui/SourceTableModelTest.java
@@ -0,0 +1,67 @@
+package org.jghill.timelinevisualizersourcesgui;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * A class for testing the SourceTableModel.
+ * 
+ * @author JGHill
+ */
+public class SourceTableModelTest {
+    
+    public SourceTableModelTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getRowCount method, of class SourceTableModel.
+     */
+    @Test
+    public void testGetRowCount() {
+
+    }
+
+    /**
+     * Test of getColumnCount method, of class SourceTableModel.
+     */
+    @Test
+    public void testGetColumnCount() {
+
+    }
+
+    /**
+     * Test of getValueAt method, of class SourceTableModel.
+     */
+    @Test
+    public void testGetValueAt() {
+
+    }
+
+    /**
+     * Test of getColumnName method, of class SourceTableModel.
+     */
+    @Test
+    public void testGetColumnName() {
+
+    }
+    
+}
