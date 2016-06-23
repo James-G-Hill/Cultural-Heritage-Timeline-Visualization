61d76f24b9620ac9323813266dc1891ad895bfa0
One <One@PC-One.lan>
2016-06-23 20:36:25 +0100

    Removed unnecessary methods, imports, comments, and renamed already programmed tests to be more explanatory.

--- TimelineVisualizerSources/test/unit/src/org/jghill/timelinevisualizersourcesgui/SourceTableModelTest.java
+++ TimelineVisualizerSources/test/unit/src/org/jghill/timelinevisualizersourcesgui/SourceTableModelTest.java
@@ -2,12 +2,9 @@ package org.jghill.timelinevisualizersourcesgui;
 
 import org.jghill.timelinevisualizersources.Source;
 import org.jghill.timelinevisualizersources.SourceCollection;
-import org.junit.After;
-import org.junit.AfterClass;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.mockito.Mockito.mock;
 
@@ -21,16 +18,7 @@ public class SourceTableModelTest {
     Source source1;
     Source source2;
     
-    public SourceTableModelTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
+    public SourceTableModelTest() {}
     
     @Before
     public void setUp() {
@@ -38,17 +26,8 @@ public class SourceTableModelTest {
         source2 = mock(Source.class);
     }
     
-    @After
-    public void tearDown() {
-        //sources.deleteSource(source1);
-        //sources.deleteSource(source2);
-    }
-
-    /**
-     * Test of getRowCount method, of class SourceTableModel.
-     */
     @Test
-    public void testGetRowCount() {
+    public void testGetRowCountReturnsCorrectNumberOfRows() {
         SourceTableModel table;
         SourceCollection sources;
         sources = SourceCollection.getInstance();
@@ -59,12 +38,9 @@ public class SourceTableModelTest {
         int rowCount = table.getRowCount();
         assertTrue("Count is "+rowCount, rowCount == 2);
     }
-
-    /**
-     * Test of getColumnCount method, of class SourceTableModel.
-     */
+    
     @Test
-    public void testGetColumnCount() {
+    public void testGetColumnCountReturnsCorrectNumberOfColumns() {
         SourceTableModel table;
         SourceCollection sources;
         sources = SourceCollection.getInstance();
@@ -73,18 +49,12 @@ public class SourceTableModelTest {
         int colCount = table.getColumnCount();
         assertTrue("Count is "+colCount, colCount == 2);
     }
-
-    /**
-     * Test of getValueAt method, of class SourceTableModel.
-     */
+    
     @Test
     public void testGetValueAt() {
         fail();
     }
-
-    /**
-     * Test of getColumnName method, of class SourceTableModel.
-     */
+    
     @Test
     public void testGetColumnName() {
         fail();
