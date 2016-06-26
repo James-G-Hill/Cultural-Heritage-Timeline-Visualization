288836e875fb9168792e13db4b76106e7af55b7e
One <One@PC-One.lan>
2016-06-26 02:47:02 +0100

    Added an action to the menu bar for opening the Source Management Tool.

---
+++ TimelineVisualizerSources/src/org/jghill/timelinevisualizersourcesgui/SourceManagementOpen.java
@@ -0,0 +1,25 @@
+package org.jghill.timelinevisualizersourcesgui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle.Messages;
+
+@ActionID(
+        category = "Tools",
+        id = "org.jghill.timelinevisualizersourcesgui.SourceManagementOpen"
+)
+@ActionRegistration(
+        displayName = "#CTL_SourceManagementOpen"
+)
+@ActionReference(path = "Menu/Tools", position = 0)
+@Messages("CTL_SourceManagementOpen=Source Management")
+public final class SourceManagementOpen implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO implement action body
+    }
+}
