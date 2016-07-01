6215b6e32c744ac652336a23656e0fd5603f8d41
One <One@PC-One.lan>
2016-07-01 23:03:40 +0100

    Minor changes.

---
+++ TimelineVisualizerSources/src/org/jghill/timelinevisualizersourcesgui/SourceDetailsPanel.java
@@ -0,0 +1,10 @@
+package org.jghill.timelinevisualizersourcesgui;
+
+/**
+ * An interface for the panel that sits within SourceManagementNew.
+ * 
+ * @author JGHill
+ */
+public interface SourceDetailsPanel {
+    
+}
