8aa2998fb274259f1c3d9177d0044887e64d4057
One <One@PC-One.lan>
2016-07-02 19:29:52 +0100

    Added a close button.

--- TimelineVisualizerSources/src/org/jghill/timelinevisualizersourcesgui/SourceManagementTool.java
+++ TimelineVisualizerSources/src/org/jghill/timelinevisualizersourcesgui/SourceManagementTool.java
@@ -32,6 +32,7 @@ public class SourceManagementTool extends javax.swing.JDialog {
         SourceManagementDeleteButton = new javax.swing.JButton();
         SourceManagementScrollPane = new javax.swing.JScrollPane();
         SourceManagementSourceTable = new javax.swing.JTable();
+        SourceManagementCloseButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle(org.openide.util.NbBundle.getMessage(SourceManagementTool.class, "SourceManagementTool.title")); // NOI18N
@@ -76,6 +77,13 @@ public class SourceManagementTool extends javax.swing.JDialog {
         SourceManagementSourceTable.setPreferredSize(new java.awt.Dimension(380, 140));
         SourceManagementScrollPane.setViewportView(SourceManagementSourceTable);
 
+        org.openide.awt.Mnemonics.setLocalizedText(SourceManagementCloseButton, org.openide.util.NbBundle.getMessage(SourceManagementTool.class, "SourceManagementTool.SourceManagementCloseButton.text")); // NOI18N
+        SourceManagementCloseButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                SourceManagementCloseButtonActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -83,14 +91,16 @@ public class SourceManagementTool extends javax.swing.JDialog {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(SourceManagementScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(SourceManagementNewButton)
                         .addGap(18, 18, 18)
                         .addComponent(SourceManagementEditButton)
                         .addGap(18, 18, 18)
-                        .addComponent(SourceManagementDeleteButton))
-                    .addComponent(SourceManagementScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 380, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addComponent(SourceManagementDeleteButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(SourceManagementCloseButton, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -101,7 +111,8 @@ public class SourceManagementTool extends javax.swing.JDialog {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(SourceManagementNewButton)
                     .addComponent(SourceManagementEditButton)
-                    .addComponent(SourceManagementDeleteButton))
+                    .addComponent(SourceManagementDeleteButton)
+                    .addComponent(SourceManagementCloseButton))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -124,7 +135,12 @@ public class SourceManagementTool extends javax.swing.JDialog {
         }
     }//GEN-LAST:event_SourceManagementDeleteButtonActionPerformed
 
+    private void SourceManagementCloseButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SourceManagementCloseButtonActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_SourceManagementCloseButtonActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton SourceManagementCloseButton;
     private javax.swing.JButton SourceManagementDeleteButton;
     private javax.swing.JButton SourceManagementEditButton;
     private javax.swing.JButton SourceManagementNewButton;
