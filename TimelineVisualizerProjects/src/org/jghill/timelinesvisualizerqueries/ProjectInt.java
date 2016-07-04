db1b67fa18dbe83dca9dd110ada47e903463b72e
One <One@PC-One.lan>
2016-07-04 19:04:16 +0100

    Created an interface for projects.

--- TimelineVisualizerProjects/src/org/jghill/timelinesvisualizerqueries/ProjectInt.java
+++ TimelineVisualizerProjects/src/org/jghill/timelinesvisualizerqueries/ProjectInt.java
@@ -1,5 +1,8 @@
 package org.jghill.timelinesvisualizerqueries;
 
+import org.jghill.timelinevisualizerentitiescollection.EntitiesCollection;
+import org.jghill.timelinevisualizerqueriescollection.QueriesCollection;
+
 /**
  * An interface for Projects.
  * 
@@ -7,6 +10,16 @@ package org.jghill.timelinesvisualizerqueries;
  */
 public interface ProjectInt {
     
+    public String getName();
     
+    public void setName(String name);
+    
+    public EntitiesCollection getEntitiesCollection();
+    
+    public void setEntitiesCollection(EntitiesCollection collection);
+    
+    public QueriesCollection getQueriesCollection();
+    
+    public void setQueriesCollection(QueriesCollection collection);
     
 }
