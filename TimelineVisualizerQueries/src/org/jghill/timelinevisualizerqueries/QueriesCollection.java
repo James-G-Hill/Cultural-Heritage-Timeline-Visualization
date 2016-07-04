52998180404f9d341663a489ed2215b31d9904cc
One <One@PC-One.lan>
2016-07-04 18:47:44 +0100

    Renamed this class.

---
+++ TimelineVisualizerQueries/src/org/jghill/timelinevisualizerqueries/QueriesCollection.java
@@ -0,0 +1,37 @@
+package org.jghill.timelinevisualizerqueries;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * A collection for holding a set of AbstractQueries.
+ * 
+ * @author JGHill
+ */
+public class QueriesCollection {
+    
+    private final SortedSet<AbstractQuery> collection;
+    
+    public QueriesCollection() {
+        collection = new TreeSet<>();
+    }
+    
+    /**
+     * A method for adding new queries into the collection.
+     * 
+     * @param q A query.
+     */
+    public void addQuery(AbstractQuery q) {
+        collection.add(q);
+    }
+    
+    /**
+     * A method for returning a count of the queries in the collection.
+     * 
+     * @return the count of queries.
+     */
+    public int getCount() {
+        return collection.size();
+    }
+    
+}
