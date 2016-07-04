79db61ce273c7e9fd9025e95d44383faa7d941a3
One <One@PC-One.lan>
2016-07-04 19:18:39 +0100

    Created a second constructor that doesn't require a name and added javadoc for both constructors.

--- TimelineVisualizerProjects/src/org/jghill/timelinesvisualizerqueries/Project.java
+++ TimelineVisualizerProjects/src/org/jghill/timelinesvisualizerqueries/Project.java
@@ -14,11 +14,33 @@ public class Project implements ProjectInt {
     private EntitiesCollection entities;
     private QueriesCollection queries;
     
+    /**
+     * A constructor accepting collections.
+     * 
+     * @param entities an entities collection.
+     * @param queries a queries collection.
+     */
+    public void Project(
+            EntitiesCollection entities,
+            QueriesCollection queries) {
+        
+        this.entities = entities;
+        this.queries = queries;
+    }
+    
+    /**
+     * A constructor accepting collections.
+     * 
+     * @param name a name for the project.
+     * @param entities an entities collection.
+     * @param queries a queries collection.
+     */
     public void Project(
             String name,
             EntitiesCollection entities,
             QueriesCollection queries) {
         
+        this.name = name;
         this.entities = entities;
         this.queries = queries;
     }
