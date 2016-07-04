4069ce036f57c432970d78f247b054e892dd1feb
One <One@PC-One.lan>
2016-07-04 18:35:02 +0100

    Created a new QueryCollection class.

---
+++ TimelineVisualizerQueries/src/org/jghill/timelinevisualizerqueries/QueryCollection.java
@@ -0,0 +1,12 @@
+package org.jghill.timelinevisualizerqueries;
+
+/**
+ * A collection for holding a set of AbstractQueries.
+ * 
+ * @author JGHill
+ */
+public class QueryCollection {
+    
+    
+    
+}
