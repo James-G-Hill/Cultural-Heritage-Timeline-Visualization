fe592af786b1032a16965d6b92ce44d9a1b139d0
One <One@PC-One.lan>
2016-07-04 19:06:19 +0100

    Created an implementation shell of the ProjectInt class.

---
+++ TimelineVisualizerProjects/src/org/jghill/timelinesvisualizerqueries/Project.java
@@ -0,0 +1,43 @@
+package org.jghill.timelinesvisualizerqueries;
+
+import org.jghill.timelinevisualizerentitiescollection.EntitiesCollection;
+import org.jghill.timelinevisualizerqueriescollection.QueriesCollection;
+
+/**
+ * A project for holding data from a set of queries.
+ * 
+ * @author JGHill
+ */
+public class Project implements ProjectInt {
+
+    @Override
+    public String getName() {
+        
+    }
+
+    @Override
+    public void setName(String name) {
+        
+    }
+
+    @Override
+    public EntitiesCollection getEntitiesCollection() {
+        
+    }
+
+    @Override
+    public void setEntitiesCollection(EntitiesCollection collection) {
+        
+    }
+
+    @Override
+    public QueriesCollection getQueriesCollection() {
+        
+    }
+
+    @Override
+    public void setQueriesCollection(QueriesCollection collection) {
+        
+    }
+    
+}
