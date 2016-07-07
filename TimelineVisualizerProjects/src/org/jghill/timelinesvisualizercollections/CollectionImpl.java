fd1370f5e56099bdf82cc42ca60acb2d1f08a660
One <One@PC-One.lan>
2016-07-07 19:37:42 +0100

    Changed name to CollectionImpl.

---
+++ TimelineVisualizerProjects/src/org/jghill/timelinesvisualizercollections/CollectionImpl.java
@@ -0,0 +1,70 @@
+package org.jghill.timelinesvisualizercollections;
+
+import org.jghill.timelinevisualizerentitiescollection.EntitiesCollection;
+import org.jghill.timelinevisualizerqueriescollection.QueriesCollection;
+
+/**
+ * A project for holding data from a set of queries.
+ * 
+ * @author JGHill
+ */
+public class CollectionImpl implements Collection, Comparable<CollectionImpl> {
+
+    private String name;
+    private String notes;
+    private final EntitiesCollection entities;
+    private final QueriesCollection queries;
+    
+    /**
+     * A constructor accepting containers.
+     * 
+     * @param name a name for the project.
+     * @param entities an entities container.
+     * @param queries a queries container.
+     */
+    public CollectionImpl(
+            String name,
+            EntitiesCollection entities,
+            QueriesCollection queries) {
+        
+        this.name = name;
+        this.entities = entities;
+        this.queries = queries;
+    }
+    
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getNotes() {
+        return notes;
+    }
+    
+    @Override
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+    
+    @Override
+    public EntitiesCollection getEntitiesCollection() {
+        return entities;
+    }
+
+    @Override
+    public QueriesCollection getQueriesCollection() {
+        return queries;
+    }
+
+    @Override
+    public int compareTo(CollectionImpl o) {
+        return name.compareTo(o.getName());
+    }
+    
+}
