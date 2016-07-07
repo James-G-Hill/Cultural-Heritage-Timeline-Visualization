10cc0be2ee9df568488d1c01870b840a6abcadd1
One <One@PC-One.lan>
2016-07-07 20:54:55 +0100

    Changed to reflect renamed Collection class.

--- TimelineVisualizerProjects/src/org/jghill/timelinesvisualizercollectionsgui/CollectionNew.java
+++ TimelineVisualizerProjects/src/org/jghill/timelinesvisualizercollectionsgui/CollectionNew.java
@@ -3,15 +3,15 @@ package org.jghill.timelinesvisualizercollectionsgui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.time.LocalDateTime;
-import org.jghill.timelinesvisualizercollections.Collection;
+import org.jghill.timelinesvisualizercollections.CollectionImpl;
 import org.jghill.timelinevisualizerentitiescollection.EntitiesCollection;
 import org.jghill.timelinevisualizerqueriescollection.QueriesCollection;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
 import org.openide.util.NbBundle.Messages;
-import org.jghill.timelinesvisualizercollections.CollectionInt;
 import org.jghill.timelinesvisualizercollectionscontainer.CollectionContainer;
+import org.jghill.timelinesvisualizercollections.Collection;
 
 @ActionID(
         category = "File",
@@ -28,7 +28,7 @@ public final class CollectionNew implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         EntitiesCollection entities = new EntitiesCollection();
         QueriesCollection queries = new QueriesCollection();
-        CollectionInt coll = new Collection("New " + LocalDateTime.now(), entities, queries);
+        Collection coll = new CollectionImpl("New " + LocalDateTime.now(), entities, queries);
         CollectionTopComponent collTC = new CollectionTopComponent();
         collTC.setCollection(coll);
         collTC.open();
