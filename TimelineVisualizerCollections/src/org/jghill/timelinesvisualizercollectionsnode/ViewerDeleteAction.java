b1576dcd89d325a89d02afbae44dc4fd8b1f73c3
One <One@PC-One.lan>
2016-07-10 15:10:53 +0100

    Created an action for deleting a Collection.

---
+++ TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollectionsnode/ViewerDeleteAction.java
@@ -0,0 +1,33 @@
+package org.jghill.timelinesvisualizercollectionsnode;
+
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import org.jghill.timelinesvisualizercollections.CanDelete;
+import org.jghill.timelinesvisualizercollections.Collection;
+import org.openide.util.Lookup;
+
+/**
+ * Deletes a Collection.
+ * 
+ * @author JGHill
+ */
+public class ViewerDeleteAction extends AbstractAction {
+    
+    private final Collection coll;
+    
+    public ViewerDeleteAction(Lookup lu) {
+        coll = lu.lookup(Collection.class);
+        putValue(NAME, "Delete");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(coll != null) {
+            CanDelete deleter = coll.getLookup().lookup(CanDelete.class);
+            if(deleter != null){
+                deleter.delete(coll);
+            }
+        }
+    }
+
+}
