3a3443065ef8a25cf0f5ffdbd3675c268f9bfd94
One <One@PC-One.lan>
2016-07-10 05:04:02 +0100

    Added to represent an ability.

---
+++ TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollections/CanOpen.java
@@ -0,0 +1,12 @@
+package org.jghill.timelinesvisualizercollections;
+
+/**
+ * An interface representing the opening of an object.
+ * 
+ * @author JGHill
+ */
+public interface CanOpen {
+    
+    public void open(Collection coll);
+    
+}
