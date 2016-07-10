f5b2d955a7afba8e24e9066e2a528b6416eb73ea
One <One@PC-One.lan>
2016-07-10 05:06:55 +0100

    Changed to action the method in the passed collection.

--- TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollectionsnode/ViewerOpenAction.java
+++ TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollectionsnode/ViewerOpenAction.java
@@ -2,8 +2,8 @@ package org.jghill.timelinesvisualizercollectionsnode;
 
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
+import org.jghill.timelinesvisualizercollections.CanOpen;
 import org.jghill.timelinesvisualizercollections.Collection;
-import org.jghill.timelinesvisualizercollectionsgui.CollectionTopComponent;
 import org.openide.util.Lookup;
 
 /**
@@ -23,9 +23,10 @@ public class ViewerOpenAction extends AbstractAction {
     @Override
     public void actionPerformed(ActionEvent e) {
         if(coll != null) {
-            CollectionTopComponent collTC = new CollectionTopComponent();
-            collTC.setCollection(coll);
-            collTC.open();
+            CanOpen opener = coll.getLookup().lookup(CanOpen.class);
+            if(opener != null){
+                opener.open(coll);
+            }
         }
     }
     
