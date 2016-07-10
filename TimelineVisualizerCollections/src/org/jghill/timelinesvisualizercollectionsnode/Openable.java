ddd80d71043078ee884a94c121034fd4b80d39b2
One <One@PC-One.lan>
2016-07-10 04:41:15 +0100

    Added new Openable interface.

---
+++ TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollectionsnode/Openable.java
@@ -0,0 +1,12 @@
+package org.jghill.timelinesvisualizercollectionsnode;
+
+/**
+ * An interface representing the opening of an object.
+ * 
+ * @author JGHill
+ */
+interface Openable {
+    
+    public void open();
+    
+}
