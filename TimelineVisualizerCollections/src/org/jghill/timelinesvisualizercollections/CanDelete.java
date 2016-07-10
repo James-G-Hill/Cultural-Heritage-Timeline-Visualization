f88494db4141f7596279af46ce266a552fccd9e2
One <One@PC-One.lan>
2016-07-10 15:11:13 +0100

    Created an interface for a deletable node.

---
+++ TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollections/CanDelete.java
@@ -0,0 +1,12 @@
+package org.jghill.timelinesvisualizercollections;
+
+/**
+ * An interface representing the deletion of an object.
+ * 
+ * @author JGHill
+ */
+public interface CanDelete {
+    
+    public void delete(Collection coll);
+    
+}
