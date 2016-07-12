7f7340ad675d670cdc73007abf7e0b2d274935c2
One <One@PC-One.lan>
2016-07-12 21:21:25 +0100

    Created a QuerySettings class.

---
+++ TimelineVisualizerQueries/src/org/jghill/timelinesvisualizerqueriesbuilder/QuerySettings.java
@@ -0,0 +1,13 @@
+package org.jghill.timelinesvisualizerqueriesbuilder;
+
+/**
+ * An object for holding settings for queries that can be passed from the
+ * CollectionTopComponent to the QueryBuilder.
+ * 
+ * @author JGHill
+ */
+public class QuerySettings {
+    
+    
+    
+}
