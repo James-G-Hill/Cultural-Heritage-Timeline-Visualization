32bf37e4a556bb241cdfd9f4a921be10a4159f96
One <One@PC-One.lan>
2016-07-16 21:44:52 +0100

    Added methods for building the entities collection.

--- TimelineVisualizerQueries/src/org/jghill/timelinevisualizerqueries/SPARQLQueryShell.java
+++ TimelineVisualizerQueries/src/org/jghill/timelinevisualizerqueries/SPARQLQueryShell.java
@@ -1,5 +1,12 @@
 package org.jghill.timelinevisualizerqueries;
 
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.jghill.timelinevisualizerentities.ManMadeObject;
 import org.jghill.timelinevisualizerentitiescollection.EntitiesCollection;
 
 /**
@@ -9,7 +16,7 @@ import org.jghill.timelinevisualizerentitiescollection.EntitiesCollection;
  */
 public class SPARQLQueryShell extends QueryShell {
     
-    String queryString;
+    private final String queryString;
     
     public SPARQLQueryShell(String queryString, String name) {
         this.queryString = queryString;
@@ -18,7 +25,40 @@ public class SPARQLQueryShell extends QueryShell {
 
     @Override
     public EntitiesCollection run() {
-        
+        return getResults(QueryFactory.create(queryString));
+    }
+    
+    /**
+     * Executes the query and returns the results.
+     * 
+     * @param query the QueryFactory.
+     * @return the entities.
+     */
+    private EntitiesCollection getResults(Query query) {
+        try(QueryExecution qexec = QueryExecutionFactory.create(query)) {
+            ResultSet results = qexec.execSelect();
+            return buildEntities(results);
+        }
+    }
+    
+    /**
+     * Builds the collection of entities.
+     * 
+     * @param results the ResultSet
+     * @return the entities.
+     */
+    private EntitiesCollection buildEntities(ResultSet results) {
+        EntitiesCollection entities = new EntitiesCollection();
+        for(; results.hasNext();) {
+            
+            QuerySolution soln = results.nextSolution();
+            ManMadeObject thing;
+            thing = new ManMadeObject(
+                    soln.getLiteral("owner")
+            );
+            entities.addThing(thing);
+        }
+        return entities;
     }
     
 }
