1b82dbfc4f2346e7a488c8f101688f697fbdbea0
One <One@PC-One.lan>
2016-07-19 21:35:43 +0100

    Modified getResults method to throw and exception and added a get for title in buildEntities.

--- TimelineVisualizerQueries/src/org/jghill/timelinevisualizerqueries/SPARQLQueryShell.java
+++ TimelineVisualizerQueries/src/org/jghill/timelinevisualizerqueries/SPARQLQueryShell.java
@@ -1,5 +1,6 @@
 package org.jghill.timelinevisualizerqueries;
 
+import org.apache.jena.atlas.web.HttpException;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
@@ -8,8 +9,6 @@ import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
 import org.jghill.timelinevisualizerentities.ManMadeObject;
 import org.jghill.timelinevisualizerentitiescollection.EntitiesCollection;
-import org.netbeans.api.io.IOProvider;
-import org.netbeans.api.io.InputOutput;
 
 /**
  * A concrete implementation of the QueryShell class representing a SPARQL query.
@@ -19,10 +18,12 @@ import org.netbeans.api.io.InputOutput;
 public class SPARQLQueryShell extends QueryShell {
     
     private final String queryString;
+    private final String service;
     private static final String QUERY_TYPE = "SPARQL Endpoint";
     
-    public SPARQLQueryShell(String queryString, String name) {
+    public SPARQLQueryShell(String queryString, String service, String name) {
         this.queryString = queryString;
+        this.service = service;
         super.setQueryName(name);
     }
     
@@ -47,10 +48,12 @@ public class SPARQLQueryShell extends QueryShell {
      * @param query the QueryFactory.
      * @return the entities.
      */
-    private EntitiesCollection getResults(Query query) {
-        try(QueryExecution qexec = QueryExecutionFactory.create(query)) {
+    private EntitiesCollection getResults(Query query) throws HttpException {
+        try(QueryExecution qexec = QueryExecutionFactory.sparqlService(service, query)) {
             ResultSet results = qexec.execSelect();
             return buildEntities(results);
+        } catch (HttpException ex) {
+            throw new HttpException("502 Proxy Error:");
         }
     }
     
@@ -64,14 +67,15 @@ public class SPARQLQueryShell extends QueryShell {
         EntitiesCollection entities = new EntitiesCollection();
         for(; results.hasNext();) {
             QuerySolution soln = results.next();
-            output(soln.toString());
             
-            String owner = soln.getLiteral("owner").getString();
+            String title = soln.getResource("title").toString();
+            String owner = soln.getResource("owner").toString();
             
             ManMadeObject thing;
             thing = new ManMadeObject(
                     owner
             );
+            thing.setName(title);
             entities.addThing(thing);
         }
         return entities;
@@ -81,15 +85,6 @@ public class SPARQLQueryShell extends QueryShell {
     public int compareTo(Object o) {
         QueryShell query = (QueryShell) o;
         return super.getQueryName().compareTo(query.getQueryName());
-    }
-    
-    /**
-     * Outputs an explanation of the action.
-     * @param name the name of the Collection that was created.
-     */
-    private void output(String soln) {
-        InputOutput io = IOProvider.getDefault().getIO("Main", false);
-        io.getOut().println(soln);
     }
     
 }
