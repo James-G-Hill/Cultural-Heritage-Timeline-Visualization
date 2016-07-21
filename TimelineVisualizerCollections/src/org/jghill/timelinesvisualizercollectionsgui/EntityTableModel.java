faa04f5f2410241c4f81b0f623a4abb5f6bd473a
One <One@PC-One.lan>
2016-07-21 23:37:41 +0100

    Added an entitiesFlatten method and changed other methods.

--- TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollectionsgui/EntityTableModel.java
+++ TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollectionsgui/EntityTableModel.java
@@ -1,5 +1,8 @@
 package org.jghill.timelinesvisualizercollectionsgui;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import javax.swing.table.AbstractTableModel;
 import org.jghill.timelinevisualizerentities.Entities;
 import org.jghill.timelinevisualizerentitiescollection.EntitiesCollection;
@@ -12,7 +15,7 @@ import org.jghill.timelinevisualizerentitiescollection.EntitiesCollection;
 public class EntityTableModel extends AbstractTableModel {
 
     private final EntitiesCollection entitiesCollection;
-    private Entities[] things;
+    private Entities[] entities;
     
     private static final int COL_COUNT = 2;
     
@@ -23,7 +26,7 @@ public class EntityTableModel extends AbstractTableModel {
     
     @Override
     public int getRowCount() {
-        return entitiesCollection.count();
+        return entities.length;
     }
 
     @Override
@@ -35,9 +38,9 @@ public class EntityTableModel extends AbstractTableModel {
     public Object getValueAt(int rowIndex, int columnIndex) {
         switch(columnIndex){
             case 0:
-                return things[rowIndex].getName();
+                return entities[rowIndex].getName();
             case 1:
-                return things[rowIndex].getName();
+                return entities[rowIndex].getName();
             default:
                 return null;
         }
@@ -65,10 +68,9 @@ public class EntityTableModel extends AbstractTableModel {
      * Updates the array fed from the data table.
      */
     private void updateDataTable() {
-        things = new Entities[entitiesCollection.count()];
-        entitiesCollection.getCollectionSet().toArray(things);
+        List flat = entitiesFlatten(entitiesCollection);
+        entities = (Entities[]) flat.toArray(new Entities[flat.size()]);
     }
-    
     
     /**
      * Returns an entity from the underlying collection.
@@ -77,7 +79,25 @@ public class EntityTableModel extends AbstractTableModel {
      * @return The entity at the row.
      */
     public Entities returnQuery(int index) {
-        return things[index];
+        return entities[index];
+    }
+    
+    /**
+     * Flattens the entities collection into a single list.
+     * 
+     * @param coll the EntitiesCollection.
+     * @return a list containing all internal non collections.
+     */
+    private List<Entities> entitiesFlatten(EntitiesCollection coll) {
+        List<Entities> arr = new ArrayList<>();
+        for (Entities e : coll.getCollectionSet()) {
+            if(e.getClass() == EntitiesCollection.class) {
+                arr.addAll(entitiesFlatten((EntitiesCollection)e));
+            } else {
+                arr.add(e);
+            }
+        }
+        return arr;
     }
     
 }
