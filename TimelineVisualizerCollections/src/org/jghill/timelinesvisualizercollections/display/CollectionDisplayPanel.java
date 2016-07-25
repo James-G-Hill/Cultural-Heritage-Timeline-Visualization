14f30cb53557469437f0a65b8db882ec703faf43
One <One@PC-One.lan>
2016-07-25 20:17:24 +0100

    Added class.

---
+++ TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollections/display/CollectionDisplayPanel.java
@@ -0,0 +1,14 @@
+package org.jghill.timelinesvisualizercollections.display;
+
+import javax.swing.JPanel;
+
+/**
+ * A Panel for displaying the results.
+ * 
+ * @author JGHill
+ */
+public class CollectionDisplayPanel extends JPanel {
+    
+    public CollectionDisplayPanel() {}
+    
+}
