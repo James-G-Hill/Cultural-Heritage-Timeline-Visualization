e11866f0d4b4b121203d505b60b8c2dab3a1ac69
One <One@PC-One.lan>
2016-07-25 21:46:49 +0100

    Added code to the paintComponent and allowed an array to be set.

--- TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollections/display/TimeLine.java
+++ TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollections/display/TimeLine.java
@@ -1,5 +1,7 @@
 package org.jghill.timelinesvisualizercollections.display;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import javax.swing.JPanel;
 
 /**
@@ -9,6 +11,30 @@ import javax.swing.JPanel;
  */
 public class TimeLine extends JPanel {
     
+    private int[] intervals;
+    
     public TimeLine() {}
+    
+    public void setArray(int[] intervals) {
+        this.intervals = intervals;
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        int height = getHeight();
+        int width = getWidth();
+        
+        g.setColor(Color.BLACK);
+        g.drawLine(10, 10, width - 10, 10);
+        
+        int lineLength = width - 20;
+        for(int i = 0; i < intervals.length; i++) {
+            int x = 10 + ((lineLength / intervals.length) * (i + 1));
+            g.drawLine(x, x, x, x + 30);
+        }
+        
+    }
     
 }
