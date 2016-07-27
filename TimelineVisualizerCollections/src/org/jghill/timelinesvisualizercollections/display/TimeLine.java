754c494d84e29f82d6dfe3fd9fb6892ab5a9d12d
One <One@PC-One.lan>
2016-07-27 21:03:54 +0100

    Modified paintScale and addLabel methods.

--- TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollections/display/TimeLine.java
+++ TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollections/display/TimeLine.java
@@ -2,6 +2,7 @@ package org.jghill.timelinesvisualizercollections.display;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -10,6 +11,10 @@ import javax.swing.JPanel;
  * @author JGHill
  */
 public class TimeLine extends JPanel {
+    
+    private final static int INDENT = 10;
+    private final static int UPNOTCH = 5;
+    private final static int DOWNNOTCH = 15;
     
     private int[] intervals;
     
@@ -33,15 +38,23 @@ public class TimeLine extends JPanel {
         int width = getWidth();
         
         g.setColor(Color.BLACK);
-        g.drawLine(10, 10, width - 10, 10);
+        g.drawLine(INDENT, INDENT, width - INDENT, INDENT);
         
-        int lineLength = width - 20;
+        int lineLength = width - (INDENT * 2);
         int x;
         for(int i = 0; i < intervals.length; i++) {
-            x = 10 + ((lineLength / intervals.length) * i);
-            g.drawLine(x, x - 5, x, x + 15);
+            x = INDENT + ((lineLength / intervals.length) * i);
+            g.drawLine(x, 10 - UPNOTCH, x, 10 + DOWNNOTCH);
+            addLabel(x, intervals[i]);
         }
         
+    }
+    
+    private void addLabel(int x, int year) {
+        JLabel yearLabel = new JLabel();
+        yearLabel.setLocation(x + 5, x + 5);
+        yearLabel.setText("Interval: " + String.valueOf(year));
+        this.add(yearLabel);
     }
     
 }
