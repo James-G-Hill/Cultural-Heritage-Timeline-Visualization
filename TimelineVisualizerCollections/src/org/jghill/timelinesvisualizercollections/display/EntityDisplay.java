0d4eadaa724aaf761d6090f040b3f6deb3379ac9
One <One@PC-One.lan>
2016-07-28 20:02:01 +0100

    Added getImage method and variables.

--- TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollections/display/EntityDisplay.java
+++ TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollections/display/EntityDisplay.java
@@ -2,8 +2,10 @@ package org.jghill.timelinesvisualizercollections.display;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import static java.lang.Math.min;
 import javax.swing.JPanel;
 import org.jghill.timelinevisualizerentities.Entities;
+import org.jghill.timelinevisualizerentities.PhysicalThing;
 
 /**
  * A component for displaying entities.
@@ -12,27 +14,51 @@ import org.jghill.timelinevisualizerentities.Entities;
  */
 public class EntityDisplay extends JPanel {
     
-    Entities entity;
-    BufferedImage fullSize;
-    BufferedImage reSize;
+    private static final int MAX_DIMENSION = 100;
+    
+    private final Entities entity;
+    
+    private BufferedImage fullSize;
+    private int thumbWidth;
+    private int thumbHeight;
     
     public EntityDisplay(Entities entity) {
         this.entity = entity;
+        
     }
     
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        getImage();
         paintBase(g);
-        paintPhoto(g);
+        paintImage(g);
         paintConnection(g);
     }
     
-    private void paintBase(Graphics g) {
+    private void getImage() {
+        PhysicalThing pt = (PhysicalThing) entity;
+        fullSize = pt.getImage();
+        
+        int x = fullSize.getWidth();
+        int y = fullSize.getHeight();
+        double ratio = x / y;
+        
+        boolean xOversized = (x > MAX_DIMENSION);
+        boolean yOversized = (y > MAX_DIMENSION);
+        
+        if(xOversized || yOversized) {
+            thumbWidth = (int) min(MAX_DIMENSION, ratio * MAX_DIMENSION);
+            thumbHeight = (int) min(MAX_DIMENSION, MAX_DIMENSION / ratio);
+        }
         
     }
     
-    private void paintPhoto(Graphics g) {
+    private void paintBase(Graphics g) {
+        this.setSize(WIDTH, HEIGHT);
+    }
+    
+    private void paintImage(Graphics g) {
         
     }
     
