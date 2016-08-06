e7f54fdec73e01f56fd8123f50e9208e7c24032a
One <One@PC-One.lan>
2016-08-06 03:26:40 +0100

    Added a Lookup, Javadoc and a method for adding to the Lookup.

--- TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollections/container/CollectionContainer.java
+++ TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollections/container/CollectionContainer.java
@@ -1,13 +1,16 @@
 package org.jghill.timelinesvisualizercollections.container;
 
+import java.util.Collections;
 import java.util.SortedSet;
 import java.util.TreeSet;
 import org.jghill.timelinesvisualizercollections.Collection;
 import org.jghill.timelinesvisualizercollections.node.CollectionChildren;
+import org.openide.util.Lookup;
+import org.openide.util.lookup.AbstractLookup;
+import org.openide.util.lookup.InstanceContent;
 
 /**
  * A collection for holding projects.
- * 
  * @author JGHill
  */
 public class CollectionContainer {
@@ -16,11 +19,13 @@ public class CollectionContainer {
     private static final CollectionContainer CONTAINER = new CollectionContainer();
     private static final SortedSet<Collection> COLLECTION = new TreeSet<>();
     
+    private static final InstanceContent CONTENT = new InstanceContent();
+    private static final Lookup LKP = new AbstractLookup(CONTENT);
+    
     private CollectionContainer() {}
     
     /**
      * Returns the single instance of this singleton pattern.
-     * 
      * @return the single instance of this container.
      */
     public static CollectionContainer getInstance() {
@@ -29,35 +34,53 @@ public class CollectionContainer {
     
     /**
      * A method for adding collections to the container.
-     * 
      * @param coll the project to be added.
      */
-    public void addCollection(Collection coll) {
+    public static void addCollection(Collection coll) {
         COLLECTION.add(coll);
         CHILDREN.update(COLLECTION.toArray(new Collection[COLLECTION.size()]));
     }
     
     /**
      * A method for removing collections from the container.
-     * 
      * @param coll the project to be added.
      */
-    public void deleteCollection(Collection coll) {
+    public static void deleteCollection(Collection coll) {
         COLLECTION.remove(coll);
         CHILDREN.update(COLLECTION.toArray(new Collection[COLLECTION.size()]));
     }
     
     /**
      * Returns the size of the collection.
-     * 
      * @return the size of the collection.
      */
-    public int getSize() {
+    public static int getSize() {
         return COLLECTION.size();
     }
     
-    public CollectionChildren getChildren() {
+    /**
+     * Returns all the CollectionChildren.
+     * @return the CollectionChildren.
+     */
+    public static CollectionChildren getChildren() {
         return CHILDREN;
+    }
+    
+    /**
+     * Returns the Lookup for this container.
+     * @return the Lookup.
+     */
+    public static Lookup getLookup() {
+        return LKP;
+    }
+    
+    /**
+     * Adds a Collection to the lookup for the container.
+     * @param coll to be added.
+     */
+    public static void addToLookup(Collection coll) {
+        CONTENT.set(Collections.EMPTY_LIST, null);
+        CONTENT.add(coll);
     }
     
 }
