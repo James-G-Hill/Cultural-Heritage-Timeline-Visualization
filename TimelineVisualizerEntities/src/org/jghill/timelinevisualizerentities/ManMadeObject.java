f8c8840b5f442d44662b75c29099eb0d84409803
One <One@PC-One.lan>
2016-08-12 22:00:52 +0100

    Added source and query to the constructor.

--- TimelineVisualizerEntities/src/org/jghill/timelinevisualizerentities/ManMadeObject.java
+++ TimelineVisualizerEntities/src/org/jghill/timelinevisualizerentities/ManMadeObject.java
@@ -1,21 +1,23 @@
 package org.jghill.timelinevisualizerentities;
 
+import javax.management.Query;
+import javax.xml.transform.Source;
+
 /**
  * A class representing objects that have been designed &  created by humans.
- * 
  * @author JGHill
  */
 public class ManMadeObject extends PhysicalThing {
     
-    public final String currentOwner = "";
-    
     public ManMadeObject(
             String name,
             String identifier,
+            Source source,
+            Query query,
             String image,
             String timeSpan
     ) {
-        super(name, identifier, image, timeSpan);
+        super(name, identifier, source, query, image, timeSpan);
     }
     
 }
