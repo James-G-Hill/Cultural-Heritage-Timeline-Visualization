fe582a50d3206400c833358630ad4807430d372a
One <One@PC-One.lan>
2016-08-12 21:57:36 +0100

    Added source and query as variables.

--- TimelineVisualizerEntities/src/org/jghill/timelinevisualizerentities/Entities.java
+++ TimelineVisualizerEntities/src/org/jghill/timelinevisualizerentities/Entities.java
@@ -1,34 +1,62 @@
 package org.jghill.timelinevisualizerentities;
 
+import javax.management.Query;
+import javax.xml.transform.Source;
+
 /**
  * A class for representing collections & physical things as components of a
  * composition.
- * 
  * @author JGHill
  */
 abstract public class Entities implements Comparable {
     
-    private static final String NO_IDENTIFIER = "No Identifier";
-    
     private final String name;
     private final String identifier;
+    private final Source source;
+    private final Query query;
     
-    public Entities(String name) {
-        this.name = name;
-        this.identifier = NO_IDENTIFIER;
-    }
-    
-    public Entities(String name, String identifier) {
+    public Entities(
+            String name,
+            String identifier,
+            Source source,
+            Query query
+    ) {
         this.name = name;
         this.identifier = identifier;
+        this.source = source;
+        this.query = query;
     }
     
+    /**
+     * Returns the name of the entity.
+     * @return the name.
+     */
     public String getName() {
         return name;
     }
     
+    /**
+     * Returns the identifier that the institution applied to the entity.
+     * @return the identifier.
+     */
     public String getIdentifier() {
         return identifier;
+    }
+    
+    /**
+     * Returns the Source from which the entity was obtained.
+     * @return the Source.
+     */
+    public Source getSource() {
+        return source;
+    }
+    
+    /**
+     * Returns the Query which the entity was returned from.
+     * @return the Query.
+     */
+    public Query getQuery() {
+        return query;
     }
     
     @Override
