f7c2e2f3882e6742f4a25bbf19ce6b56938c9fdc
One <One@PC-One.lan>
2016-08-17 20:21:59 +0100

    Modified JavaDoc.

--- TimelineVisualizerEntities/src/org/jghill/timelinevisualizerentities/ManMadeObject.java
+++ TimelineVisualizerEntities/src/org/jghill/timelinevisualizerentities/ManMadeObject.java
@@ -2,6 +2,7 @@ package org.jghill.timelinevisualizerentities;
 
 /**
  * A class representing objects that have been designed &  created by humans.
+ * 
  * @author JGHill
  */
 public class ManMadeObject extends PhysicalThing {
