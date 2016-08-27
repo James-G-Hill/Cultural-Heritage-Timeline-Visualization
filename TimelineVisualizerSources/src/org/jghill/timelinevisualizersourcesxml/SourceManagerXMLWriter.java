96438c97ebe64378957722cde25c6b05cac3d09a
One <One@PC-One.lan>
2016-08-27 18:24:33 +0100

    Created inteface.

---
+++ TimelineVisualizerSources/src/org/jghill/timelinevisualizersourcesxml/SourceManagerXMLWriter.java
@@ -0,0 +1,10 @@
+package org.jghill.timelinevisualizersourcesxml;
+
+/**
+ * An XML writer for the Source Manager info.
+ * 
+ * @author JGHill
+ */
+public interface SourceManagerXMLWriter {
+    
+}
