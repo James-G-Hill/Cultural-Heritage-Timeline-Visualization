f459b7dfd46b5dc7e75e2ea3a7bddfd071e8e021
One <One@PC-One.lan>
2016-08-27 18:24:59 +0100

    Created implementation.

---
+++ TimelineVisualizerSources/src/org/jghill/timelinevisualizersourcesxml/SourceManagerXMLParserImpl.java
@@ -0,0 +1,10 @@
+package org.jghill.timelinevisualizersourcesxml;
+
+/**
+ * An implementation of the Source Manager XML parser.
+ * 
+ * @author JGHill
+ */
+public class SourceManagerXMLParserImpl implements SourceManagerXMLParser {
+    
+}
