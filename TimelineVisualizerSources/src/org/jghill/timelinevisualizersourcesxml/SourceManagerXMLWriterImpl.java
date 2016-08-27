9e2829a1c8542ed203929a5eba0bbbdaa8bb8b00
One <One@PC-One.lan>
2016-08-27 18:25:21 +0100

    Created implementation.

---
+++ TimelineVisualizerSources/src/org/jghill/timelinevisualizersourcesxml/SourceManagerXMLWriterImpl.java
@@ -0,0 +1,10 @@
+package org.jghill.timelinevisualizersourcesxml;
+
+/**
+ * An implementation of the Source Manager XML writer.
+ * 
+ * @author JGHill
+ */
+public class SourceManagerXMLWriterImpl implements SourceManagerXMLWriter {
+    
+}
