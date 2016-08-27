6787e5c5570aec69953caa01e33ee44f3eeb159f
One <One@PC-One.lan>
2016-08-27 18:38:18 +0100

    Filled out the methods required for this XML parser.

--- TimelineVisualizerSources/src/org/jghill/timelinevisualizersourcesxml/SourceManagerXMLWriter.java
+++ TimelineVisualizerSources/src/org/jghill/timelinevisualizersourcesxml/SourceManagerXMLWriter.java
@@ -1,10 +1,24 @@
 package org.jghill.timelinevisualizersourcesxml;
 
+import org.jghill.timelinevisualizersources.SourceCollection;
+
 /**
  * An XML writer for the Source Manager info.
  * 
  * @author JGHill
  */
 public interface SourceManagerXMLWriter {
+    
+    /**
+     * Builds a DOM document for this Source Manager.
+     * 
+     * @param collection of Sources to be built from.
+     */
+    public void build(SourceCollection collection);
+    
+    /**
+     * Prints a DOM document to the system.
+     */
+    public void print();
     
 }
