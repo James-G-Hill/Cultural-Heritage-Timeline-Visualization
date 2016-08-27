23f01fec08414a25fd586096ef5936d59d1b061b
One <One@PC-One.lan>
2016-08-27 18:24:25 +0100

    Created inteface.

---
+++ TimelineVisualizerSources/src/org/jghill/timelinevisualizersourcesxml/SourceManagerXMLParser.java
@@ -0,0 +1,10 @@
+package org.jghill.timelinevisualizersourcesxml;
+
+/**
+ * An interface for XML parsing the Source Manager information.
+ * 
+ * @author JGHill
+ */
+public interface SourceManagerXMLParser {
+    
+}
