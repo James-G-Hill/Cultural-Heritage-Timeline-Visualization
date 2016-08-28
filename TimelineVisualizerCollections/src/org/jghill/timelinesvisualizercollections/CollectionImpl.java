1cb7bc5ac1f8466f5c2eeb7bfbfde2d5c2f7d478
One <One@PC-One.lan>
2016-08-28 18:08:16 +0100

    Changed the formatting.

--- TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollections/CollectionImpl.java
+++ TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollections/CollectionImpl.java
@@ -29,6 +29,7 @@ public class CollectionImpl implements Collection, Comparable<CollectionImpl>, L
     
     /**
      * A constructor accepting containers.
+     * 
      * @param name a name for the project.
      * @param entities an entities container.
      * @param queries a queries container.
@@ -36,7 +37,8 @@ public class CollectionImpl implements Collection, Comparable<CollectionImpl>, L
     public CollectionImpl(
             String name,
             EntitiesCollection entities,
-            QueriesCollection queries) {
+            QueriesCollection queries
+    ) {
         
         this.name = name;
         this.entities = entities;
