5811d98cbfd4f9b2c5d02814491ae5273bdf4cd0
One <One@PC-One.lan>
2016-08-28 04:13:23 +0100

    Created class.

---
+++ TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollectionxml/CollectionXMLWriterImpl.java
@@ -0,0 +1,10 @@
+package org.jghill.timelinesvisualizercollectionxml;
+
+/**
+ * An implementation of the collection writer.
+ * 
+ * @author JGHill
+ */
+public class CollectionXMLWriterImpl {
+    
+}
