dc23344a869756807721895a3cd5d2fe6b8f87b2
One <One@PC-One.lan>
2016-08-28 04:14:01 +0100

    Created class.

---
+++ TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollectionxml/CollectionXMLParserImpl.java
@@ -0,0 +1,10 @@
+package org.jghill.timelinesvisualizercollectionxml;
+
+/**
+ * An implementation of the Collection parser.
+ * 
+ * @author JGHill
+ */
+public class CollectionXMLParserImpl {
+    
+}
