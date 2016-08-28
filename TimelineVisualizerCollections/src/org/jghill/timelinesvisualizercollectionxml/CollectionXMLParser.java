1cfa59f71f8bef9c5dc6204db3fe3d1405442fc7
One <One@PC-One.lan>
2016-08-28 04:17:28 +0100

    Added basic parser.

--- TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollectionxml/CollectionXMLParser.java
+++ TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollectionxml/CollectionXMLParser.java
@@ -1,10 +1,20 @@
 package org.jghill.timelinesvisualizercollectionxml;
 
+import javax.xml.xpath.XPathExpressionException;
+import org.jghill.timelinesvisualizercollections.Collection;
+
 /**
  * A parser for collections.
  * 
  * @author JGHill
  */
 public interface CollectionXMLParser {
+    
+    /**
+     * A method for parsing Collections from XML files.
+     * 
+     * @return a Collection.
+     */
+    public Collection parseCollections() throws XPathExpressionException;
     
 }
