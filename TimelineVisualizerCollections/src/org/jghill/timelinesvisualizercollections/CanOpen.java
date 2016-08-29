514cbd8b2a3d0248b30298b68f2a61a8907acc90
One <One@PC-One.lan>
2016-08-29 10:32:42 +0100

    Modified the JavaDoc.

--- TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollections/CanOpen.java
+++ TimelineVisualizerCollections/src/org/jghill/timelinesvisualizercollections/CanOpen.java
@@ -2,10 +2,16 @@ package org.jghill.timelinesvisualizercollections;
 
 /**
  * An interface representing the opening of an object.
+ * 
  * @author JGHill
  */
 public interface CanOpen {
     
+    /**
+     * A method for opening a Collection.
+     * 
+     * @param coll the collection to be opened.
+     */
     public void open(Collection coll);
     
 }
